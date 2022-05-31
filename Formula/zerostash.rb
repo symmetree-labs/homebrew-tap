af7e8ff5c3f7db0c06799477cbf5576e6021a1dc
rsdy <p@symmetree.dev>
2022-05-31 13:04:42 +0100

    Update version

--- Formula/zerostash.rb
+++ Formula/zerostash.rb
@@ -1,8 +1,8 @@
 class Zerostash < Formula
   desc "Safe and speedy backups"
   homepage "https://github.com/symmetree-labs/zerostash"
-  url "https://github.com/symmetree-labs/zerostash/archive/refs/tags/v0.3.tar.gz"
-  sha256 "4ba955486d89341131d398ac0255a92920a0b48df71c4c04c0991046d867b224"
+  url "https://github.com/symmetree-labs/zerostash/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "e8e6801ef611b4812c7cc7b7c70cd1991b9cce799f5584e4af8fca17c77b2e27"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/symmetree-labs/zerostash.git", branch: "master"
 
@@ -14,6 +14,6 @@ class Zerostash < Formula
 
   test do
     output = shell_output("0s -V")
-    assert_match "zerostash 0.3.0", output
+    assert_match "zerostash 0.4.1", output
   end
 end
